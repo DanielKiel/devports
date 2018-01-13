e511b3d0d679d0ed9d15e2d1690d0a7bdaf040b5
Daniel Koch <daniel.koch@publicare.de>
2018-01-13 13:04:20 +0100

    removing public folder from url at a hosting provider with no access to some apcahe or nginx configs;

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: dk
+ * Date: 13.01.18
+ * Time: 13:02
+ */
 
 /**
  * Laravel - A PHP Framework For Web Artisans
@@ -21,7 +27,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +41,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
