fcf96c8f60322b829d121293e8a32797e53beec1
Daniel Koch <daniel.koch@publicare.de>
2018-01-22 11:21:22 +0100

    verif

--- resources/views/layouts/material.blade.php
+++ resources/views/layouts/material.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Google Wemaster Tools -->
-    <meta name="google-site-verification" content="jqTUkbWXUL9z1jrnFzBx8X0-GG_WzaNUyu6cv3RIHVM" />
+    <meta name="google-site-verification" content="jqTUkbWXUL9z1jrnFzBx8X0-GG_WzaNUyu6cv3RIHVM" /> 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
